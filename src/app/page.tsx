c75a1d35584ac77d822405aeecbee758e4ea4eda
Lee Mander <lee.mander@gmail.com>
2023-11-23 14:13:02 +0000

    Add root route link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Link className="underline text-red-600" href="/blogs">
+        To the blogs!
+      </Link>
+    </main>
+  );
 }
